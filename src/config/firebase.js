59cd43866f903b8228433a1b113f045b5b3f2b92
testing4992484-ui <testing4992484@gmail.com>
2026-05-20 19:46:12 +0530

    Update src/config/firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
@@ -16,4 +16,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+export const googleProvider = new GoogleAuthProvider();
 export default app;
